0f09d1a5b5674d702e1b00a177c3f2f2afc0165f
Tahseen <tassutahsee@gmail.com>
2025-08-11 17:54:25 +0530

    change port number of backend

--- backend/server.js
+++ backend/server.js
@@ -141,7 +141,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
