73d6b6d56ade27a89b33846c039ef7c7b9d33f08
Tahseen <tassutahsee@gmail.com>
2025-08-23 23:18:16 +0530

    remove unusual code

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserPlus, User, Mail, Lock } from 'lucide-react';
 import { motion } from 'framer-motion';
-const API_URL = import.meta.env.API_URL;
+
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const THEME = {
   primary: "#6D28D9",       // Deep purple
